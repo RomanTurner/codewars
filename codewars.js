8f2a303f69d79b44109a9895ab4f4e47e47c0f27
Roman Turner <rn.turner@live.com>
2021-02-13 18:09:18 -0800

    a couple more finished

--- codewars.js
+++ codewars.js
@@ -207,3 +207,99 @@ overTheRoad(3, 5) = 8 */
 function overTheRoad(address, n) {
   return n * 2 + 1 - address;
 }
+
+/**
+ 
+Collect|
+Challenge:
+
+Given a two-dimensional array, return a new array which carries over 
+only those arrays from the original, which were not empty and whose 
+items are all of the same type (i.e. homogenous). For simplicity, 
+the arrays inside the array will only contain characters and integers.
+
+Example:
+
+Given [[1, 5, 4], ['a', 3, 5], ['b'], [], ['1', 2, 3]], your 
+function should return [[1, 5, 4], ['b']].
+
+Addendum:
+
+Please keep in mind that for this kata, we assume that empty 
+arrays are not homogenous.
+
+The resultant arrays should be in the order they were originally 
+in and should not have its values changed.
+
+No implicit type casting is allowed. A subarray [1, '2'] would be 
+considered illegal and should be filtered out.
+ */
+
+
+let filterHomogenous = (a) =>
+  a.filter((b) => b.length > 0 && b.every((e) => typeof e == typeof b[0]));
+
+/**
+ * Imagine you start on the 5th floor of a building,
+ *  then travel down to the 2nd floor, then back up to the 8th floor. 
+ * You have travelled a total of 3 + 6 = 9 floors of distance.
+
+Given an array representing a series of floors you must reach
+ by elevator, return an integer representing the total distance 
+ travelled for visiting each floor in the array in order.
+
+// simple examples
+elevatorDistance([5,2,8]) = 9
+elevatorDistance([1,2,3]) = 2
+elevatorDistance([7,1,7,1]) = 18
+
+// if two consecutive floors are the same,
+//distance travelled between them is 0
+elevatorDistance([3,3]) = 0
+Array will always contain at least 2 floors. Random tests 
+will contain 2-20 elements in array, and floor values between 0 and 30.
+ */
+
+ function elevatorDistance(arr) {
+   let sum = 0;
+   for (let i = 0; i < arr.length - 1; i++) {
+     sum += Math.abs(arr[i] - arr[i + 1]);
+   }
+   return sum;
+ }
+
+/**
+ The number 89 is the first integer with more than one digit that fulfills the property partially introduced in the title of this kata. What's the use of saying "Eureka"? Because this sum gives the same number.
+
+In effect: 89 = 8^1 + 9^2
+
+The next number in having this property is 135.
+
+See this property again: 135 = 1^1 + 3^2 + 5^3
+
+We need a function to collect these numbers, that may receive two integers a, b that defines the range [a, b] (inclusive) and outputs a list of the sorted numbers in the range that fulfills the property described above.
+
+Let's see some cases:
+
+sumDigPow(1, 10) == [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+sumDigPow(1, 100) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 89]
+If there are no numbers of this kind in the range [a, b] the function should output an empty list.
+
+  sumDigPow(90, 100) == []
+  */
+ function sumDigPow(a, b) {
+   let result_arr = [];
+   for (let i = a; i < b + 1; i++) {
+     let result = 0;
+     let string = i.toString();
+     for (let k = 0; k < string.length + 1; k++) {
+       result = result + Math.pow(string.charAt(k), k + 1);
+     }
+     if (result === i) {
+       result_arr.push(result);
+     }
+   }
+
+   return result_arr;
+ }
