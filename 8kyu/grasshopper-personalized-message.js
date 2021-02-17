fcc4fea04f8cb4e8e57488348c1a6a386de2f14f
Roman Turner <rn.turner@live.com>
2021-02-17 00:03:31 -0800

    8kyu

---
+++ 8kyu/grasshopper-personalized-message.js
@@ -0,0 +1,21 @@
+/* 
+Personalized greeting
+Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+Use conditionals to return the proper message:
+
+case	return
+name equals owner	'Hello boss'
+otherwise	'Hello guest'
+*/
+
+
+function greet(name, owner) {
+  return  name === owner ? `Hello boss` : `Hello guest`;
+}
+
+function greet(name, owner) {
+  return `Hello ${name == owner ? "boss" : "guest"}`;
+}
+console.log(greet('greg', 'bryan'));
+console.log(greet('greg', 'greg'));
