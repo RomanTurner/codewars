2eecc3fcb4dbadb9eff21a91c12f669d552ebe23
Roman Turner <rn.turner@live.com>
2021-08-17 18:31:20 -0700

    7kyu 💨 Vaporcode

--- 7kyu/vaporcode.rb
+++ 7kyu/vaporcode.rb
@@ -4,8 +4,8 @@ def vaporcode(string)
   if string.empty?
       return nil;
   end
+# alternatively can string.gsub!(/\s+/, '') and then 
  string.delete(" ").upcase.chars.join"  "
 end
 
 vaporcode('dope')
-binding.pry
